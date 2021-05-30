f0a9bb7b86efe6a868dba8d6dc9380d3d311d15e
Dhruv <dhruvrawat2611@gmail.com>
2021-05-30 13:53:53 +0530

    crreated formula bar added cell and option toggles

--- script.js
+++ script.js
@@ -16,18 +16,29 @@ $(document).ready(function () {
         }
         let column = $(`<div class="column-name  colId-${i}" id="colCod-${ans}">${ans}</div>`);
         $(".column-name-container").append(column);
-        let row=$(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+        let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
         $(".row-name-container").append(row);
     }
-    for(let i=1;i<=100;i++){
-        let row=$(`<div class="cell-row"></div>`);
-        for(let j=1;j<=100;j++){
-            let colCode=$(`.colId-${j}`).attr("id").split("-")[1];
-            let column=$(` <div class="input-cell" contenteditable="true" id="row-${i}-col-${j}" data="code-${colCode}"></div>`);
+    for (let i = 1; i <= 100; i++) {
+        let row = $(`<div class="cell-row"></div>`);
+        for (let j = 1; j <= 100; j++) {
+            let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
+            let column = $(` <div class="input-cell" contenteditable="true" id="row-${i}-col-${j}" data="code-${colCode}"></div>`);
             row.append(column);
         }
         $(".input-cell-container").append(row);
     }
+    $(".align-icon").click(function () {
+        $(".align-icon.selected").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $(".style-icon").click(function () {
+        $(this).toggleClass("selected");
+    });
+
+    $(".input-cell").click(function () {
+        $(".input-cell.selected").removeClass("selected");
+        $(this).addClass("selected");
+    });
 });
-
-
