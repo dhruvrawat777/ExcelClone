9f67674079cf880c6797d2974be7b0c9043588d0
Dhruv <dhruvrawat2611@gmail.com>
2021-06-11 22:20:35 +0530

    made multiple cells selectable

--- script.js
+++ script.js
@@ -37,19 +37,60 @@ $(document).ready(function () {
         $(this).toggleClass("selected");
     });
 
-    $(".input-cell").click(function () {
-        $(".input-cell.selected").removeClass("selected");
-        $(this).addClass("selected");
+    $(".input-cell").click(function (e) {
+        if (e.ctrlKey) {
+            let [rowId, colId] = getRowCol(this);
+            if (rowId > 1) {
+                let topCellSelected = $(`#row-${rowId - 1}-col-${colId}`).hasClass("selected");
+                if (topCellSelected) {
+                    $(this).addClass("top-cell-selected");
+                    $(`#row-${rowId - 1}-col-${colId}`).addClass("bottom-cell-selected");
+                }
+            }
+            if (rowId < 100) {
+                let bottomCellSelected = $(`#row-${rowId + 1}-col-${colId}`).hasClass("selected");
+                if (bottomCellSelected) {
+                    $(this).addClass("bottom-cell-selected");
+                    $(`#row-${rowId + 1}-col-${colId}`).addClass("top-cell-selected");
+                }
+            }
+            if (colId > 1) {
+                let leftCellSelected = $(`#row-${rowId}-col-${colId - 1}`).hasClass("selected");
+                if (leftCellSelected) {
+                    $(this).addClass("left-cell-selected");
+                    $(`#row-${rowId}-col-${colId - 1}`).addClass("right-cell-selected");
+                }
+            }
+            if (colId < 100) {
+                let rightCellSelected = $(`#row-${rowId}-col-${colId + 1}`).hasClass("selected");
+                if (rightCellSelected) {
+                    $(this).addClass("right-cell-selected");
+                    $(`#row-${rowId}-col-${colId + 1}`).addClass("left-cell-selected");
+                }
+            }
+            $(this).addClass("selected");
+        }
+        else {
+            $(".input-cell.selected").removeClass("selected");
+            $(this).addClass("selected");
+        }
     });
-    $(".input-cell").dblclick(function(){
+    $(".input-cell").dblclick(function () {
         $(".input-cell.selected").removeClass("selected");
         $(this).addClass("selected");
-        $(this).attr("contenteditable","true");
+        $(this).attr("contenteditable", "true");
         $(this).focus();
     });
-    $(".input-cell-container").scroll(function(){
+    $(".input-cell-container").scroll(function () {
         $(".column-name-container").scrollLeft(this.scrollLeft);
         $(".row-name-container").scrollTop(this.scrollTop);
     })
-    
+
 });
+
+function getRowCol(ele) {
+    let idArray = $(ele).attr('id').split('-');
+    let rowId = parseInt(idArray[1]);
+    let colId = parseInt(idArray[3]);
+    return [rowId, colId];
+}
